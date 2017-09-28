bc450eebf92ab05aceed182c763dd4f8864150c5
Nikhil Jacob Jose <nikhiljacobjose89@gmail.com>
2017-09-28 15:04:14 -0500

    Add files via upload

---
+++ app/src/main/java/com/nikhiljacobjose/flashchatnewfirebase/InstantMessage.java
@@ -0,0 +1,27 @@
+package com.nikhiljacobjose.flashchatnewfirebase;
+
+/**
+ * Created by FNU on 6/26/2017.
+ */
+
+public class InstantMessage {
+
+    private String message;
+    private String author;
+
+    public InstantMessage(String message, String author) {
+        this.message = message;
+        this.author = author;
+    }
+
+    public InstantMessage() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
